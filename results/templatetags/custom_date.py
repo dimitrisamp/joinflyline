c4e6b1117f63103016bcb51f877c2ab85aba1f0e
Dimitris Amprazis <bladeofdima@gmail.com>
2019-09-01 21:35:09 +0300

    date filter

---
+++ results/templatetags/custom_date.py
@@ -0,0 +1,11 @@
+from django import template
+from django.template.defaultfilters import register
+from django.utils import dateparse
+
+register = template.Library()
+
+
+@register.filter(name='iso_date')
+def iso_date(value):
+    y = dateparse.parse_datetime(value)
+    return y
