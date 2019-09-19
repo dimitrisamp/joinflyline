e793ad5475c5708575e6633dff6dba61e069bc07
Dimitris Amprazis <bladeofdima@gmail.com>
2019-09-19 19:49:40 +0300

    More on certs

--- static/js/home.js
+++ static/js/home.js
@@ -29,8 +29,29 @@ const app = new Vue({
                 url: 'http://aviation-edge.com/v2/public/autocomplete?key=140940-4e6372&city=' + app.city,
                 success: function (data) {
                     if (data) {
-                        data = JSON.parse(data).cities;
+                       data = JSON.parse(data).cities;
                         //data = JSON.parse(data).airportsByCities;
+
+       /*                 let cities = [];
+                        cities.push(JSON.parse(data).cities);
+                        let airports = [];
+                        airports.push(JSON.parse(data).airportsByCities);
+*/
+                        //let values = airports.concat(cities);
+
+
+
+
+                       /* console.log(values)
+*/
+                    /* for(let i = 0; i <=  values[0].length; i++ ){
+                         filteredData.push(values[i]);
+                            if(values[1].length-1 !== i) {
+                                filteredData.push(values[1]);
+                            }
+                         console.log(filteredData);
+                        }
+                        data = JSON.parse(data).cities;*/
                         app.searchResultPlaces = data;
 
                     }
@@ -45,10 +66,50 @@ const app = new Vue({
             this.mySelection.splice(index, 1);
         },
 
+         getSelectedRadioValue: function(item, index) {
+           console.log(item);
+           if(item.checked === true){
+               app.form.stop = item.value;
+
+           }
+
+        },
+        setStopOverValue: function(stopOverTo, stopOverFrom) {
+            app.form.stopOverFrom = stopOverFrom+ ":" + "00";
+            app.form.stopOverTo= stopOverTo+ ":" + "00";
+        },
+
         search: () => {
             let formData = new FormData;
             app.form.departure_date = document.getElementById('departure_date').value;
             app.form.return_date = document.getElementById('return_date').value;
+
+            if(document.getElementById('stopover') != null){
+                let stopSelected = document.getElementById('stopover').value;
+                let chars = stopSelected.split(',');
+
+                let stopOverTo;
+                let stopOverFrom;
+
+                if(chars[0].length === 1){
+                    stopOverFrom = "0" +chars[0];
+                }else {
+                    stopOverFrom = chars[0];
+                }
+
+                if(chars[1].length === 1){
+                    stopOverTo = "0" +chars[1];
+                }else {
+                    stopOverTo = chars[1];
+                }
+
+                app.setStopOverValue(stopOverTo,stopOverFrom);
+            }
+
+            if(document.getElementsByName("stop") != null){
+                document.getElementsByName("stop").forEach(app.getSelectedRadioValue)
+            }
+
 
             formData.append("city_from", app.form.placeFrom);
             formData.append("city_to", app.form.placesTo);
@@ -58,6 +119,9 @@ const app = new Vue({
             formData.append("adults", app.form.valAdults);
             formData.append("infants", app.form.valInfants);
             formData.append("children", app.form.valChildren);
+            formData.append("max_stopovers", app.form.stop);
+            formData.append("stopover_from", app.form.stopOverFrom);
+            formData.append("stopover_to", app.form.stopOverTo);
 
             let object = {};
             formData.forEach(function (value, key) {
@@ -168,16 +232,14 @@ const app = new Vue({
         },
         increment: function (index) {
 
-
             app.valPassengers = app.form.valAdults + app.form.valChildren + app.form.valInfants;
 
-
-            if (app.valPassengers != 9) {
+            if (app.valPassengers !== 9) {
 
                 switch (index) {
                     case 1:
 
-                        if (app.form.valAdults != 9) {
+                        if (app.form.valAdults !== 9) {
                             app.form.valAdults++;
                             app.valPassengers++;
                         } else {
@@ -187,7 +249,7 @@ const app = new Vue({
                         return;
                     case 2:
 
-                        if (app.form.valChildren != 8) {
+                        if (app.form.valChildren !== 8) {
                             app.form.valChildren++;
                             app.valPassengers++;
                         } else {
@@ -197,7 +259,7 @@ const app = new Vue({
                         return;
                     case 3:
 
-                        if (app.form.valInfants != app.form.valAdults) {
+                        if (app.form.valInfants !== app.form.valAdults) {
                             app.form.valInfants++;
                             app.valPassengers++;
                         } else {
@@ -209,16 +271,17 @@ const app = new Vue({
             }
 
         },
+
         decrement: function (index) {
 
             app.valPassengers = app.form.valAdults + app.form.valChildren + app.form.valInfants;
 
-            if (app.valPassengers != 1) {
+            if (app.valPassengers !== 1) {
 
                 switch (index) {
                     case 1:
 
-                        if (app.form.valAdults != 1) {
+                        if (app.form.valAdults !== 1) {
                             app.form.valAdults--;
                             app.valPassengers--;
                             if (app.form.valInfants > app.form.valAdults) {
@@ -231,7 +294,7 @@ const app = new Vue({
                         app.sumTotalsPassengers();
                         return;
                     case 2:
-                        if (app.form.valChildren != 0) {
+                        if (app.form.valChildren !== 0) {
                             app.form.valChildren--;
                             app.valPassengers--;
                         } else {
@@ -240,7 +303,7 @@ const app = new Vue({
                         app.sumTotalsPassengers();
                         return;
                     case 3:
-                        if (app.form.valInfants != 0) {
+                        if (app.form.valInfants !== 0) {
                             app.form.valInfants--;
                             app.valPassengers--;
                         } else {
@@ -254,7 +317,7 @@ const app = new Vue({
         },
 
         sumTotalsPassengers: function () {
-            if (app.valPassengers == 1) {
+            if (app.valPassengers === 1) {
                 app.form.noOfPassengers = 'Passengers';
             } else {
                 app.form.noOfPassengers = app.valPassengers + ' Passengers'
@@ -268,12 +331,13 @@ const app = new Vue({
             app.city = '';
 
             let placeFrom = document.getElementById('placesFrom').value;
-            let placeTo = document.getElementById('placesTo').value
+
+            let placeTo = document.getElementById('placesTo').value;
 
             switch (app.selectionOption) {
                 case 1:
                     document.getElementById('placesModalTitle').innerText = "From";
-                    if (placeFrom == null || placeFrom == '') {
+                    if (placeFrom == null || placeFrom === '') {
                         document.getElementById('mySelectedPlace').style.display = 'none';
                     } else {
                         document.getElementById('mySelectedPlace').style.display = 'block'
@@ -284,10 +348,10 @@ const app = new Vue({
                     return;
                 case 2:
                     document.getElementById('placesModalTitle').innerText = "To";
-                    if (placeTo == null || placeTo == '') {
+                    if (placeTo == null || placeTo === '') {
                         document.getElementById('mySelectedPlace').style.display = 'none';
                     } else {
-                        document.getElementById('mySelectedPlace').style.display = 'block'
+                        document.getElementById('mySelectedPlace').style.display = 'block';
                         document.getElementById('mySelectedPlace').innerText = placeTo;
                     }
                     $('#placesModal').modal('show');
