c7ad53e97733eeea086b47e24cc1a6ec65ad3e10
Dimitris Amprazis <bladeofdima@gmail.com>
2019-10-01 23:27:44 +0300

    fix year field

--- booking/static/js/bookflight.js
+++ booking/static/js/bookflight.js
@@ -1,31 +1,36 @@
 $(function() {
+
+    var yearSelect = document.querySelector('#yearofbirth');
+
+    var date = new Date();
+    var year = date.getFullYear();
+  
+    // Make this year, and the 100 years before it available in the year <select>
+    for(var i = 0; i <= 100; i++) {
+        var option = document.createElement('option');
+        option.textContent = year-i;
+        yearSelect.appendChild(option);
+    }
+    
     $('#booking_flight').submit(function(event) {
 
         /* stop form from submitting normally */
         event.preventDefault();
-        $(this).valid();
+
         let passengers = [];
-        $('#passengers').children('div.whiteBg').each(function() {
-            passengers.push({
-                'givennames': $($(this).find('input#givennames')[0]).val(),
-                'surenames': $($(this).find('input#surenames')[0]).val(),
-                'nationality': $($(this).find('select#nationality')[0]).val(),
-                'birthday': $($(this).find('select#monthofbirth')[0]).val()
-                                + '/' + $($(this).find('select#dayofbirth')[0]).val()
-                                + '/' + $($(this).find('input#yearofbirth')[0]).val(),
+        if($('#booking_flight').valid() && $('#passengerForm').valid()) {
+            $('#passengers').children('div.whiteBg').each(function() {
+                passengers.push({
+                    'givennames': $($(this).find('input#givennames')[0]).val(),
+                    'surenames': $($(this).find('input#surenames')[0]).val(),
+                    'nationality': $($(this).find('select#nationality')[0]).val(),
+                    'birthday': $($(this).find('select#monthofbirth')[0]).val()
+                                    + '/' + $($(this).find('select#dayofbirth')[0]).val()
+                                    + '/' + $($(this).find('input#yearofbirth')[0]).val(),
+                });
             });
-        });
-        console.log(passengers);
-        
-        // const whiteBgs = document.querySelectorAll(".whiteBg");
-        // 
-        // Object.entries(whiteBgs).map((p) => {
-        //     console.log(p);
-        //     passengers.push({
-        //         "givennames" : p.givennames,
-        //         "suernames": p.suernames
-        //     })
-        // });
+            console.log(passengers);
+        }
     });
 });
 
