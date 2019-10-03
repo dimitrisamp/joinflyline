eb66a6a3f1a8fd8f2d22e235552c1855a69a5d35
Dimitris Amprazis <bladeofdima@gmail.com>
2019-10-03 07:50:49 +0600

    Fix duration interval calculation

--- results/templatetags/time_duration.py
+++ results/templatetags/time_duration.py
@@ -1,14 +1,20 @@
 from django import template
 from django.utils import dateparse
 
-register = template.Library()
 
-
-@register.filter(name='time_interval')
 def time_interval(value):
-    x = dateparse.parse_datetime(value['local_departure'])
-    y = dateparse.parse_datetime(value['local_arrival'])
+    x = dateparse.parse_datetime(value['utc_departure'])
+    y = dateparse.parse_datetime(value['utc_arrival'])
     z = y-x
-    hours = int(z.seconds/3600)
-    minutes = int(z.seconds/60 - (hours*60))
-    return str(hours)+"h "+str(minutes)+"m "
+    return secs2hm(z.seconds)
+
+
+def secs2hm(value):
+    hours = int(value / 3600)
+    minutes = int(value / 60 - (hours * 60))
+    return str(hours) + "h " + str(minutes) + "m"
+
+
+register = template.Library()
+register.filter("secs2hm", secs2hm)
+register.filter("time_interval", time_interval)
