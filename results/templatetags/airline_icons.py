a5279121bd810e8e9d4bc172835c62ede1c8f196
Dimitris Amprazis <bladeofdima@gmail.com>
2019-10-06 13:59:39 +0600

    Refactor: use dict instead of multiple if-return statements

--- results/templatetags/airline_icons.py
+++ results/templatetags/airline_icons.py
@@ -3,25 +3,20 @@ from django import template
 register = template.Library()
 
 
-@register.filter(name='aircon')
+AIRLINE_ICONS = {
+    "DL": "delta",
+    "AS": "alaska",
+    "NK": "spirit",
+    "B6": "jetblue",
+    "F9": "frontier",
+    "G4": "allegiant",
+    "UA": "united",
+    "AA": "american",
+    "WN": "southwest",
+    "SY": "suncountry",
+}
+
+
+@register.filter(name="aircon")
 def aircon(value):
-    if value == 'DL':
-        return "delta"
-    if value == 'AS':
-        return 'alaska'
-    if value == 'NK':
-        return 'spirit'
-    if value == 'B6':
-        return 'jetblue'
-    if value == 'F9':
-        return 'frontier'
-    if value == 'G4':
-        return 'allegiant'
-    if value == 'UA':
-        return 'united'
-    if value == 'AA':
-        return 'american'
-    if value == 'WN':
-        return 'southwest'
-    if value == 'SY':
-        return 'suncountry'
+    return AIRLINE_ICONS.get(value)
