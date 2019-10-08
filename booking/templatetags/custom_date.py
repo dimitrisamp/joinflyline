a8769e7a491ecf71a24265509faeadde281f4f8e
Dimitris Amprazis <bladeofdima@gmail.com>
2019-10-09 04:06:30 +0600

    Implement quick filters on search results

--- booking/templatetags/custom_date.py
+++ booking/templatetags/custom_date.py
@@ -4,6 +4,8 @@ from django.utils import dateparse
 register = template.Library()
 
 
-@register.filter(name='iso_date')
 def iso_date(value):
     return dateparse.parse_datetime(value)
+
+
+register.filter('iso_date', iso_date)
