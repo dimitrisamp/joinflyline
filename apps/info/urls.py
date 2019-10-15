9d365c3c8e3ea00f7556063d531d2a81fe810889
Dimitris Amprazis <bladeofdima@gmail.com>
2019-10-15 15:26:22 +0600

    Add destinations & plans pages

--- apps/info/urls.py
+++ apps/info/urls.py
@@ -1,14 +1,20 @@
 from django.urls import path
 
-from apps.info.views import news_view, about_view, feedback_view, team_view, partners_view, how_it_works_view, corporate_how_it_works_view ,faq_view
+import apps.info.views as V
 
 urlpatterns = [
-    path('news', news_view, name="news"),
-    path('about', about_view, name="about"),
-    path('feedback', feedback_view, name="feedback"),
-    path('team', team_view, name="team"),
-    path('partners', partners_view, name="partners"),
-    path('how-it-works', how_it_works_view, name="how-it-works"),
-    path('corporate-how-it-works', corporate_how_it_works_view, name="corporate-how-it-works"),
-    path('faq', faq_view, name="faq")
+    path("news", V.news_view, name="news"),
+    path("about", V.about_view, name="about"),
+    path("feedback", V.feedback_view, name="feedback"),
+    path("team", V.team_view, name="team"),
+    path("partners", V.partners_view, name="partners"),
+    path("how-it-works", V.how_it_works_view, name="how-it-works"),
+    path(
+        "corporate-how-it-works",
+        V.corporate_how_it_works_view,
+        name="corporate-how-it-works",
+    ),
+    path("faq", V.faq_view, name="faq"),
+    path("plans", V.plans_view, name="plans"),
+    path("destinations", V.destinations_view, name="destinations"),
 ]
--- apps/info/views.py
+++ apps/info/views.py
@@ -5,42 +5,42 @@ def news_view(request):
     context = {
         'title': 'Wanderift news'
     }
-    return render(request, 'news.html', context)
+    return render(request, 'info/news.html', context)
 
 
 def about_view(request):
     context = {
         'title': 'About us'
     }
-    return render(request, 'about.html', context)
+    return render(request, 'info/about.html', context)
 
 
 def feedback_view(request):
     context = {
         'title': 'Feedback'
     }
-    return render(request, 'feedback.html', context)
+    return render(request, 'info/feedback.html', context)
 
 
 def team_view(request):
     context = {
         'title': 'Wanderift Team'
     }
-    return render(request, 'team.html', context)
+    return render(request, 'info/team.html', context)
 
 
 def partners_view(request):
     context = {
         'title': 'Wanderift Partners'
     }
-    return render(request, 'partners.html', context)
+    return render(request, 'info/partners.html', context)
 
 
 def how_it_works_view(request):
     context = {
         'title': 'How it Works'
     }
-    return render(request, 'personal-how-it-works.html', context)
+    return render(request, 'info/personal-how-it-works.html', context)
 
 
 def corporate_how_it_works_view(request):
@@ -54,4 +54,18 @@ def faq_view(request):
     context = {
         "title": "Wanderift faq"
     }
-    return render(request, "faq.html", context)
+    return render(request, "info/faq.html", context)
+
+
+def destinations_view(request):
+    context = {
+        "title": "Destinations"
+    }
+    return render(request, "info/destinations.html", context)
+
+
+def plans_view(request):
+    context = {
+        "title": "Plans"
+    }
+    return render(request, "info/plans.html", context)
