131f1033e73367817dfcd49ffa3630d43dc30ffe
Dimitris Amprazis <bladeofdima@gmail.com>
2019-10-16 03:24:13 +0600

    WIP: link fixing

--- apps/corporate/urls.py
+++ apps/corporate/urls.py
@@ -5,5 +5,4 @@ urlpatterns = [
     path('corporate', corporate_view, name="corporate"),
     path('corporate-trips', trips_view, name="trips"),
     path('manage-corporate-trips', ManageTripsView.as_view(), name="manage-trips"),
-    path('corp-how-it-works', how_it_works, name="corp-how-it-works")
 ]
