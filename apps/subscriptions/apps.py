78cf2ffc73252275386ed32d6c4753d5aad50fb1
Dimitris Amprazis <bladeofdima@gmail.com>
2019-10-17 02:30:22 +0600

    Navbar fix

--- apps/subscriptions/apps.py
+++ apps/subscriptions/apps.py
@@ -1,5 +1,16 @@
 from django.apps import AppConfig
+from django.contrib.auth import get_user_model
+
+
+def is_subscriber(self):
+    if not hasattr(self, 'subscriptions'):
+        return False
+    return self.subscriptions.tokens > 0
 
 
 class SubscriptionsConfig(AppConfig):
     name = 'apps.subscriptions'
+
+    def ready(self):
+        User = get_user_model()
+        User.add_to_class('is_subscriber', is_subscriber)
