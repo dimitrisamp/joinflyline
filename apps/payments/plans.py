4e48d85478ffb151094d1e39b53730a8731ddf56
Dimitris Amprazis <bladeofdima@gmail.com>
2019-10-19 17:45:31 +0600

    Merge branch 'hotfix/zach-verbiage' into develop
    # Conflicts:
    #	apps/info/templates/info/personal-how-it-works.html

--- apps/payments/plans.py
+++ apps/payments/plans.py
@@ -65,6 +65,8 @@ def plan_calc(plan):
     if plan['annual']:
         result['price_annual'] = result['price_mo_annual'] * 12
         result['savings'] = result['price_mo']*12 - result['price_annual']
+    result['credits_per_month'] = f"{result['credits'] / 12:.1f}".rstrip('0').rstrip('.')
+    result['flight_num_per_month'] = f"{result['credits'] / 24:.1f}".rstrip('0').rstrip('.')
     result['flight_num'] = result['credits'] // 2
     return result
 
