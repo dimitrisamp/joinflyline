b281357029712c3e70061f5ba056debc3f0fdfc6
Dimitris Amprazis <bladeofdima@gmail.com>
2019-10-27 03:24:18 +0600

    Make search_results view return JSON data

--- apps/results/views.py
+++ apps/results/views.py
@@ -71,8 +71,7 @@ class CityAutocomplete(View):
                 )
 
 
-class ResultsView(TemplateView):
-    template_name = "results/results.html"
+class ResultsView(View):
 
     param_mapping = {
         "fly_from": "placeFrom",
@@ -87,9 +86,8 @@ class ResultsView(TemplateView):
     }
 
     def get(self, request, *args, **kwargs):
-        demo = request.GET.get('demo')
-        self.demo = bool(demo) and demo.lower() in ('true', 1)
-        return super().get(request, *args, **kwargs)
+        context_data = self.get_context_data()
+        return JsonResponse(context_data)
 
     def save_flight_data(self, flight, search_result):
         booking_token = flight["booking_token"]
@@ -123,13 +121,6 @@ class ResultsView(TemplateView):
         else:
             return ["results/results.html"]
 
-    def validate(self, search_params):
-        if self.request.user.is_subscriber() or self.demo:
-            city_from = AIRPORT_TO_CITY.get(search_params["fly_from"])
-            city_to = AIRPORT_TO_CITY.get(search_params["fly_to"])
-            if city_to not in ADJACENCY.get(city_from, set()):
-                raise Http404(f'The route {city_from} -> {city_to} is not available')
-
     def get_search_params(self):
         request = self.request
         search_params = {k: request.GET[v] for k, v in self.param_mapping.items()}
@@ -150,23 +141,7 @@ class ResultsView(TemplateView):
 
     def get_filter_params(self, search_params):
         GET = self.request.GET
-        request = self.request
         filter_params = {k: GET.get(k) for k in FILTER_KEYS if k in GET}
-
-        if request.user.is_subscriber or self.demo:
-            selected_airlines = request.GET.get("select_airlines")
-            if not selected_airlines:
-                airlines = S.SUBSCRIBER_AIRLINES
-            else:
-                airlines = (
-                    set(selected_airlines.upper().split(",")) & S.SUBSCRIBER_AIRLINES
-                )
-            filter_params["select_airlines"] = ",".join(airlines)
-            filter_params["price_to"] = (
-                S.PRICE_LIMIT_SUBSCRIBER
-                if search_params["flight_type"] == "oneway"
-                else S.PRICE_LIMIT_SUBSCRIBER * 2 + 150
-            )
         return filter_params
 
     def process_response(self, data, search_item):
@@ -193,7 +168,7 @@ class ResultsView(TemplateView):
                         ).total_seconds()
                     )
         quick_filters_data = self.get_quick_filters_data(data["data"])
-        return airlines, quick_filters_data
+        return list(airlines), quick_filters_data
 
     def query_endpoint(self, search_params, filter_params, sort_params, limit):
         search_query = {"limit": limit, "apikey": S.KIWI_API_KEY, "curr": "USD"}
@@ -236,12 +211,9 @@ class ResultsView(TemplateView):
             "filter_params": filter_params,
             "search_params": search_params,
             "sort": sort_params,
-            "demo": self.demo,
             "city_airports": [o for o in CITY_STATE_COUNTRY_AIRPORT if
                               o[0] in ADJACENCY]
         }
-        if self.request.is_ajax():
-            self.validate(search_params)
-            data = self.query_endpoint(search_params, filter_params, sort_params, limit)
-            context_data.update(data)
+        data = self.query_endpoint(search_params, filter_params, sort_params, limit)
+        context_data.update(data)
         return context_data
--- static/js/home.js
+++ static/js/home.js
@@ -28,7 +28,7 @@ function getCityInputData(k) {
 
 
 const app = new Vue({
-    el: '#main',
+    el: '#dealform',
     delimiters: ['{(', ')}'],
     data: {
         searchResultPlaces: [],
@@ -56,6 +56,7 @@ const app = new Vue({
             'C': 'Business',
             'F': 'First Class'
         },
+        searchResults: false,
         form: {
             noOfPassengers: "Passengers",
             destinationTypeId: 'return',
