b8d871b79dd2e9ea3240bde3cf0bc0860cbd93a8
Dimitris Amprazis <bladeofdima@gmail.com>
2019-11-08 18:25:48 +0200

    Celery configuration

--- wanderift/celery.py
+++ wanderift/celery.py
@@ -1,8 +1,9 @@
+from __future__ import absolute_import, unicode_literals
 import os
 from celery import Celery
 
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'wanderift.settings')
 
-celery_app = Celery('wanderift')
-celery_app.config_from_object('django.conf:settings', namespace='CELERY')
-celery_app.autodiscover_tasks()
+app = Celery('wanderift')
+app.config_from_object('django.conf:settings', namespace='CELERY')
+app.autodiscover_tasks()
--- wanderift/settings/local.py
+++ wanderift/settings/local.py
@@ -11,4 +11,6 @@ INTERNAL_IPS = [
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
+CELERY_TASK_ALWAYS_EAGER = True
+
 SITE_URL = os.getenv("SITE_URL", "http://127.0.0.1:8000")
