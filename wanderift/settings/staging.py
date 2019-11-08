f246d9d6b9306f4bfd08cc1982d7f0db497a4858
Dimitris Amprazis <bladeofdima@gmail.com>
2019-11-09 01:44:57 +0600

    WIP

--- wanderift/settings/staging.py
+++ wanderift/settings/staging.py
@@ -1,12 +1,13 @@
 from .base import *
 
-INSTALLED_APPS += [
-    'debug_toolbar'
+INSTALLED_APPS += ["debug_toolbar"]
+
+MIDDLEWARE = [
+    "debug_toolbar.middleware.DebugToolbarMiddleware",
+    *MIDDLEWARE
 ]
 
-INTERNAL_IPS = [
-    '127.0.0.1'
-]
+INTERNAL_IPS = ["127.0.0.1"]
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
