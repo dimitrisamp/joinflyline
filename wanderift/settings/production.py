c83090ae6c5d4b0b5492e27d02295163f2bc7abc
Dimitris Amprazis <bladeofdima@gmail.com>
2019-11-12 19:34:10 +0600

    Use separate db

--- wanderift/settings/production.py
+++ wanderift/settings/production.py
@@ -8,9 +8,9 @@ DEBUG = False
 sentry_sdk.init(dsn=SENTRY_DSN, integrations=[DjangoIntegration()])
 DEFAULT_FILE_STORAGE = "storages.backends.gcloud.GoogleCloudStorage"
 STATICFILES_STORAGE = "storages.backends.gcloud.GoogleCloudStorage"
-GS_BUCKET_NAME = "joinflyline"
+GS_BUCKET_NAME = "joinflyline-staging"
 GS_DEFAULT_ACL = "publicRead"
 GS_CACHE_CONTROL = "max-age=120"
 MEDIA_ROOT = "media"
 
-SITE_URL = os.getenv("SITE_URL", "https://joinflyline.com")
+SITE_URL = os.getenv("SITE_URL", "https://stating.joinflyline.com")
