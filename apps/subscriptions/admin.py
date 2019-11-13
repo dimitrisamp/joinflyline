13c6d29254bac847695b02de8b5fe75bcf507eb1
Dimitris Amprazis <bladeofdima@gmail.com>
2019-11-14 02:12:28 +0600

    Admin fix (cleanup)

--- apps/subscriptions/admin.py
+++ apps/subscriptions/admin.py
@@ -1,9 +1,5 @@
 from django.contrib import admin
 from apps.subscriptions.models import Subscriptions
-from django.db.models import Sum, Count
-
-from . import models
-
 
 
 class SubscriptionsAdmin(admin.ModelAdmin):
@@ -11,25 +7,6 @@ class SubscriptionsAdmin(admin.ModelAdmin):
     list_display_links = ("plan", "user")
     search_fields = ["plan"]
     list_per_page = 25
-
-
-@admin.register(models.SubscriptionsSummary)
-class SubscriptionsSummaryAdmin(admin.ModelAdmin):
-    def changelist_view(self, request, extra_context=None):
-        response = super().changelist_view(request, extra_context=extra_context)
-        try:
-            qs = response.context_data["cl"].queryset
-        except (AttributeError, KeyError):
-            return response
-
-        metrics = {"total": Count("plan"), "total_payment": Sum("price")}
-        response.context_data["summary"] = list(
-            qs.values("plan").annotate(**metrics).order_by("-total_payment")
-        )
-        response.context_data["summary_total"] = dict(qs.aggregate(**metrics))
-        return response
-
-    change_list_template = "admin/subscription_summary_change_list.html"
 
 
 admin.site.register(Subscriptions, SubscriptionsAdmin)
