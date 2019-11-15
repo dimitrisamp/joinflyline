32227f59cc526553116f5e7ccfba57fff0d9ae73
Dimitris Amprazis <bladeofdima@gmail.com>
2019-11-15 08:24:27 +0600

    WIP: Add `learn more to router`

--- apps/home/static/home/js/vue/router.js
+++ apps/home/static/home/js/vue/router.js
@@ -1,11 +1,13 @@
 import {Home} from "../home.js";
 import {SavingsExplained} from "./savings-explained.js";
 import {Wizard} from "../wizard.js";
+import {LearnMore} from "./learn-more.js";
 
 const routes = [
     {path: '/', component: Home},
     {path: '/savings-explained/', component: SavingsExplained},
-    {path: '/get-started/', component: Wizard}
+    {path: '/get-started/', component: Wizard},
+    {path: '/learn-more/', component: LearnMore},
 ];
 
 export const router = new VueRouter({
