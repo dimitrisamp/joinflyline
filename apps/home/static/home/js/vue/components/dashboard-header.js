5bae14efd75359af9ea19a3ef16acd7b1e3cdd66
Dimitris Amprazis <bladeofdima@gmail.com>
2019-11-17 00:15:08 +0100

    finish up the dashboad with all the screens included RWD

--- apps/home/static/home/js/vue/components/dashboard-header.js
+++ apps/home/static/home/js/vue/components/dashboard-header.js
@@ -1,4 +1,9 @@
 export const DashboardHeader = Vue.component('dashboard-header', {
     template: '#vue-dashboard-header-template',
     delimiters: ['[[', ']]'],
+    computed: {
+        toggleSidebar() {
+            return this.$store.getters.toggleSidebar;
+        }
+    }
 });
--- apps/home/static/home/js/vue/components/sidebar.js
+++ apps/home/static/home/js/vue/components/sidebar.js
@@ -1,5 +1,15 @@
 export const Sidebar = {
     template: '#vue-sidebar-template',
     delimiters: ['[[', ']]'],
-    name: 'sidebar'
+    name: 'sidebar',
+    watch: {
+        '$route' () {
+            this.$store.dispatch('toggleSidebar')
+        }
+    },
+    computed: {
+        toggleSidebar() {
+            return this.$store.getters.toggleSidebar;
+        }
+    }
 };
