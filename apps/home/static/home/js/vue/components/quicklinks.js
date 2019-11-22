cbc0deed201e2256d3b325a5d62044439953e1f2
Dimitris Amprazis <bladeofdima@gmail.com>
2019-11-23 04:46:30 +0600

    Quicklinks functionality

--- apps/home/static/home/js/vue/components/quicklinks.js
+++ apps/home/static/home/js/vue/components/quicklinks.js
@@ -1,23 +1,46 @@
-import {secs2hm} from '../../utils.js';
+import { secs2hm, pick, stripAirlines } from "../../utils.js";
+import { legacyAirlines, lowcostAirlines } from "../../airlineCodes.js";
 
-Vue.component('quicklinks', {
-    template: '#vue-quicklinks-template',
-    props: ['data', 'sort', 'airlinesFilter'],
-    delimiters: ['{(', ')}'],
-    computed: {
-        ...Vuex.mapState(['user']),
-        columns() {
-          const additionalCol = this.user && this.user.anonymous? {date: 'Earliest'} : {};
-          return {
-            price: 'Cheapest', 
-            quality: 'Recommended', 
-            duration: 'Quickest',
-            ...additionalCol
-          }
-        }
+
+Vue.component("quicklinks", {
+  template: "#vue-quicklinks-template",
+  props: ["data", "sort", "airlinesFilter"],
+  delimiters: ["{(", ")}"],
+  computed: {
+    ...Vuex.mapState(["user", "form"]),
+    airlineFilters() {
+      const legacySet = new Set(legacyAirlines);
+      const lowcostSet = new Set(lowcostAirlines);
+      const legacyAvailable = this.form.airlines.filter(o =>
+        legacySet.has(o.code)
+      );
+      const lowcostAvailable = this.form.airlines.filter(o =>
+        lowcostSet.has(o.code)
+      );
+      return {
+        legacy:
+          legacyAvailable.length > 0
+            ? stripAirlines(pick(legacyAvailable).name)
+            : null,
+        lowcost:
+          lowcostAvailable.length > 0
+            ? stripAirlines(pick(lowcostAvailable).name)
+            : null
+      };
     },
-    methods: {
-        secs2hm,
-        ...Vuex.mapActions(['sortResultsBy', 'applyAirlinesFilter'])
+    columns() {
+      const additionalCol =
+        this.user && this.user.anonymous ? { date: "Earliest" } : {};
+      return {
+        price: "Cheapest",
+        quality: "Recommended",
+        duration: "Quickest",
+        ...additionalCol
+      };
     }
+  },
+  methods: {
+    secs2hm,
+    ...Vuex.mapActions(["sortResultsBy", "applyAirlinesFilter"])
+  }
 });
