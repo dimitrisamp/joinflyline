33da5a181bd80089fc5a9386884eb70d32ae9bfd
Dimitris Amprazis <bladeofdima@gmail.com>
2019-11-23 19:57:11 +0600

    fix(UI): add manually removing modal overlay

--- apps/home/static/home/js/vue/components/search-results-popup.js
+++ apps/home/static/home/js/vue/components/search-results-popup.js
@@ -1,4 +1,11 @@
+import { removeBModalOverlay } from '../../utils.js';
+
 export const SearchResultsPopup = Vue.component('search-results-popup', {
     template: '#vue-search-results-popup-template',
     delimiters: ['[[', ']]'],
+    methods: {
+        handleGetStarted() {
+            removeBModalOverlay();
+        }
+    }
 });
