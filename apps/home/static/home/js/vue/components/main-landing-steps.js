863916deff49915c074c4e8a61e7912a4b79c62a
Dimitris Amprazis <bladeofdima@gmail.com>
2019-11-27 02:44:51 +0100

    finish up the layout and dashboard header

--- apps/home/static/home/js/vue/components/main-landing-steps.js
+++ apps/home/static/home/js/vue/components/main-landing-steps.js
@@ -1,4 +1,25 @@
 export const MainLandingSteps = Vue.component('main-landing-steps', {
     template: '#vue-main-landing-steps-template',
     delimiters: ['[[', ']]'],
+    watch: {
+      $mq: {
+        handler: 'reverseBackground',
+      }
+    },
+    mounted() {
+      this.reverseBackground();
+    },
+    methods: {
+      reverseBackground() {
+        const stepsSmallBlue = document.querySelectorAll('.js-steps-small-blue');
+        const stepsLargeBlue = document.querySelectorAll('.js-steps-large-blue');
+        if ( this.$mq == 'sm' ) {
+          stepsSmallBlue.forEach(el => el.classList.add('steps--background-blue'))
+          stepsLargeBlue.forEach(el => el.classList.remove('steps--background-blue'))
+        } else {
+          stepsSmallBlue.forEach(el => el.classList.remove('steps--background-blue'))
+          stepsLargeBlue.forEach(el => el.classList.add('steps--background-blue'))
+        }
+      }
+    }
 });
