3934be4b2a40f6eb76feffed16dbb2e884eb4841
Dimitris Amprazis <bladeofdima@gmail.com>
2019-11-29 04:02:01 +0100

    styling booking confirmation page

--- apps/home/static/home/js/vue/components/dashboard.js
+++ apps/home/static/home/js/vue/components/dashboard.js
@@ -1,7 +1,20 @@
 import {SearchForm} from "./search-form.js";
+import { store } from "../store.js";
 
 export const Dashboard = Vue.component("dashboard", {
   template: "#vue-dashboard-template",
+  watch: {
+    $route() {
+      if(this.$route.path === '/dashboard/booking') {
+       this.$store.commit('hideDashboardNav')
+      } else {
+        this.$store.commit('showDashboardNav')
+      }
+    }
+  },
+  computed: {
+    ...Vuex.mapState(['showDashboardNavigation'])
+  },
   methods: {
     proceedToSearchResults() {
       this.$router.push({name: 'results'});
