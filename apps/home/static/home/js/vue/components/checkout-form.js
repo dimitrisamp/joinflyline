067653c242a1a1f097176f26c2c6591bd89a628f
Dimitris Amprazis <bladeofdima@gmail.com>
2019-11-30 22:44:07 +0600

    Generate baggage parameter according to new api

--- apps/home/static/home/js/vue/components/checkout-form.js
+++ apps/home/static/home/js/vue/components/checkout-form.js
@@ -1,5 +1,10 @@
 export const CheckoutForm = Vue.component('checkout-form', {
     props: ['form', 'total_price'],
     template: '#vue-checkout-form-template',
+    methods: {
+        book() {
+            this.$emit('book');
+        }
+    },
     delimiters: ['[[', ']]'],
 });
