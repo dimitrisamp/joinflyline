fed95b26643769f9112b7914b6b1008be92f5451
Dimitris Amprazis <bladeofdima@gmail.com>
2019-12-02 23:05:14 +0600

    Make plans smart on homepage

--- apps/home/static/home/js/vue/components/subscription-plans.js
+++ apps/home/static/home/js/vue/components/subscription-plans.js
@@ -1,4 +1,21 @@
-export const SubscriptionPlans = Vue.component('subscription-plans', {
-    template: '#vue-subscription-plans-template',
-    delimiters: ['{(', ')}'],
+export const SubscriptionPlans = Vue.component("subscription-plans", {
+  template: "#vue-subscription-plans-template",
+  delimiters: ["[[", "]]"],
+  data() {
+    return {
+      planTabs: [
+        {group: "basic", label: "FlyLine Basic", label_mobile: "Basic", plans: ["basic", "basic-plus"]},
+        {group: "pro", label: "FlyLine Pro", label_mobile: "Pro", plans: ["pro", "pro-plus"]}
+      ],
+      selectedIndex: 0
+    };
+  },
+  methods: {
+    setSelectedIndex(i) {
+      this.selectedIndex = i;
+    }
+  },
+  computed: {
+    ...Vuex.mapState(['plans']),
+  }
 });
