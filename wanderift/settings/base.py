38614429bff3b4188e9b181c3672ce41822012e3
Dimitris Amprazis <bladeofdima@gmail.com>
2019-12-03 17:37:14 +0600

    Remove suit

--- wanderift/settings/base.py
+++ wanderift/settings/base.py
@@ -36,7 +36,7 @@ ALLOWED_HOSTS = ["0.0.0.0", "127.0.0.1", "localhost", "*"]
 
 INSTALLED_APPS = [
     # 'django_extensions',
-    "suit",
+    # "suit",
     "django.contrib.admin",
     "django.contrib.auth",
     "django.contrib.contenttypes",
