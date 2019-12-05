987e1c46009bfda8869b4cc01d6d8dcd28c4c896
Dimitris Amprazis <bladeofdima@gmail.com>
2019-12-05 07:20:33 +0600

    Make plans smart, data driven, fix links

--- apps/home/static/home/js/vue/components/membership-explained.js
+++ apps/home/static/home/js/vue/components/membership-explained.js
@@ -1,4 +1,26 @@
-export const MembershipExplained = Vue.component('membership-explained', {
-    template: '#vue-membership-explained-template',
-    delimiters: ['[[', ']]'],
+export const MembershipExplained = Vue.component("membership-explained", {
+  template: "#vue-membership-explained-template",
+  delimiters: ["[[", "]]"],
+  data() {
+    return {
+      tabs: [
+        {
+          title: "0 - 6 Round Trips",
+          plan: "basic"
+        },
+        {
+          title: "6 - 9 Round Trips",
+          plan: "basic-plus"
+        },
+        {
+          title: "9+ Round Trips",
+          plan: "pro"
+        }
+      ],
+      selectedIndex: 0,
+    };
+  },
+  computed: {
+    ...Vuex.mapState(['plans']),
+  }
 });
