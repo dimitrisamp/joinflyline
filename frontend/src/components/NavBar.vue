2ae62f7145a72bf652f37299609b6a0b547d1e13
Dimitris Amprazis <bladeofdima@gmail.com>
2020-01-10 22:09:04 +0600

    Fixes

--- frontend/src/components/NavBar.vue
+++ frontend/src/components/NavBar.vue
@@ -16,9 +16,7 @@
       >
         <span class="navbar-toggler-icon" />
       </button>
-      <div
-        :class="{ collapse: !showNavigation && $mq === 'sm'}"
-      >
+      <div :class="{ collapse: !showNavigation && $mq === 'sm' }">
         <div class="navbar-nav ml-auto">
           <div class="nav-item">
             <router-link
