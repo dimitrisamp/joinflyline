1696c8afc5970efa11323162c21e59631692bf9f
Dimitris Amprazis <bladeofdima@gmail.com>
2020-01-13 17:05:08 +0600

    feat: set header sticky

---
+++ frontend/src/components/Header.vue
@@ -0,0 +1,21 @@
+<template>
+  <header class="header">
+    <nav-bar />
+  </header>
+</template>
+
+<script>
+import NavBar from "./NavBar";
+
+export default {
+  components: {
+    NavBar
+  }
+};
+</script>
+
+<style>
+body {
+  padding-top: 72px;
+}
+</style>
